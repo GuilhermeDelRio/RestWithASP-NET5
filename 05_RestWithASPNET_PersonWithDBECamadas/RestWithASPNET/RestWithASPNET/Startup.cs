571f232a39eb54b93104b2d28e71ad298401d1e2
Guilherme Del Rio <guilherme@gplpm.com.br>
2021-08-19 20:10:56 -0300

    Modelo arquitetural com camadas

---
+++ 05_RestWithASPNET_PersonWithDBECamadas/RestWithASPNET/RestWithASPNET/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Business;
+using RestWithASPNET.Business.Implementations;
+using System;
+using RestWithASPNET.Repository;
+using RestWithASPNET.Repository.Implementations;
+
+namespace RestWithASPNET {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services) {
+
+            services.AddControllers();
+
+            // Conexão com o banco
+            var connection = Configuration["MySQLConnection:MySQLConnectionString"];
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, serverVersion));
+
+            // Versionamento da API
+            services.AddApiVersioning();
+
+            // Injeção de dependências
+            services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+            services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}

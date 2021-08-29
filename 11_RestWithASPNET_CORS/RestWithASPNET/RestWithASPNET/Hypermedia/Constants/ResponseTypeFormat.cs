c6b7fc47b5a3c51290c1bdca17d7a216856c081c
Guilherme Del Rio <guilherme@gplpm.com.br>
2021-08-29 14:24:33 -0300

    Adicionando CORS

---
+++ 11_RestWithASPNET_CORS/RestWithASPNET/RestWithASPNET/Hypermedia/Constants/ResponseTypeFormat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Hypermedia.Constants {
+    public sealed class ResponseTypeFormat {
+        public const string DefaultGet = "aplication/json";
+        public const string DefaultPost = "aplication/json";
+        public const string DefaultPut = "aplication/json";
+        public const string DefaultPatch = "aplication/json";
+    }
+}

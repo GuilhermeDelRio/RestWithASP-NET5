7a8c75e161c6ece03d3e37ca52f033973642fbcc
Guilherme Del Rio <guilherme@gplpm.com.br>
2021-09-03 20:16:26 -0300

    Trabalhando com arquivos

---
+++ 15_RestWithASPNET_Arquivos/RestWithASPNET/RestWithASPNET/Repository/Generic/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using RestWithASPNET.Model.Base;
+
+namespace RestWithASPNET.Repository {
+    public interface IRepository<T> where T : BaseEntity {
+        T Create(T item);
+        T FindById(long id);
+        T Update(T item);
+        void Delete(long id);
+        List<T> FindAll();
+        bool Exists(long id);
+        List<T> FindWithPagedSearch(string query);
+        int GetCount(string query);
+    }
+}

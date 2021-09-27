b4234228554a0db5ec915e23f85daaf049ad9df0
Guilherme Del Rio <guilherme@gplpm.com.br>
2021-09-27 18:31:36 -0300

    Azure

---
+++ 17_RestWithASPNET_Azure/RestWithASPNET/RestWithASPNET/Business/IFileBusiness.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using RestWithASPNET.Data.VO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Business {
+    public interface IFileBusiness {
+        public byte[] GetFile(string file);
+        public Task<FileDetailsVO> SaveFileToDisk(IFormFile file);
+        public Task<List<FileDetailsVO>> SaveFilesToDisk(IList<IFormFile> file);
+    }
+}
---
+++ 17_RestWithASPNET_Azure/RestWithASPNET/RestWithASPNET/Business/IPersonBusiness.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using RestWithASPNET.Data.VO;
+using RestWithASPNET.Hypermedia.Utils;
+
+namespace RestWithASPNET.Business {
+    public interface IPersonBusiness {
+        PersonVO Create(PersonVO person);
+        PersonVO FindById(long id);
+        PersonVO Update(PersonVO person);
+        PersonVO Disable(long id);
+        void Delete(long id);
+        List<PersonVO> FindAll();
+        List<PersonVO> FindByName(string firstName, string lastName);
+        PagedSearchVO<PersonVO> FindWithPagedSearch(string name, string sortDirection, int pageSize, int page);
+    }
+}
